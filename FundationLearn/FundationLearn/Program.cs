9bcadd9b26d10b469d47ced32e7fb67ba5726d1f
CHDong <raycdut@hotmail.com>
2015-06-30 17:12:19 +0800

    modif

--- FundationLearn/FundationLearn/Program.cs
+++ FundationLearn/FundationLearn/Program.cs
@@ -10,23 +10,22 @@ namespace FundationLearn
     {
         public static int X;
 
-        static A()   //4,执行后到5，
+        static A()   //2,执行后到5，
         {
-            X = B.Y + 1;//5,????
+            X = B.Y + 1;//1
         }
     }
 
     internal class B
     {
-        public static int Y = A.X + 1;//3.先执行A类中的构造函数，4。
+        public static int Y = A.X + 1;//1.先执行A类中的构造函数，3.->2
 
-        static B()          /*2.如果前面有初始值的静态字段，
-                             * 先按照文本顺序先执行带初始值的静态字段，3.*/
+        static B()          /*4.->1*/
         {
             Y = 1;
         }
 
-        private static void Main()  /*1.如果Main该类中有静态的构造函数，
+        private static void Main()  /*5.如果Main该类中有静态的构造函数，
                              * 就先执行该静态构造函数，跳到2.*/
         {
             Console.WriteLine("X={0},Y={1}", A.X, B.Y);
